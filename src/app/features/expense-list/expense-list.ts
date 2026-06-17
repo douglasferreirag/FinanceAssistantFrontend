6438c8ef8709ff92ac14b7aac8a4e77601f9cde7
douglasferreirag <douglas.ferreirag@gmail.com>
2026-06-17 13:49:12 -0300

    component expense list

---
+++ src/app/features/expense-list/expense-list.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Expense } from '../../models/expense.model';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { NavigationService } from '../../utils/NavigationService';
+
+@Component({
+  selector: 'app-expense-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './expense-list.html',
+  styleUrls: ['./expense-list.scss'],
+})
+export class ExpenseListComponent implements OnInit {
+  expenses: Expense[] = [];
+
+  constructor(private http: HttpClient, public navigation: NavigationService) {}
+
+  ngOnInit(): void {
+    this.fetchExpenses(); // chama o back assim que a página carrega
+
+  }
+
+  expenses$!: Observable<Expense[]>;
+
+  private fetchExpenses(): void {
+    this.expenses$ = this.http.get<Expense[]>('http://localhost:8080/api/expenses/findAll');
+  }
+
+
+}
